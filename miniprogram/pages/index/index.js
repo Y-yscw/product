79f88b65648bde3fb21ec8f13ad63ec66c9c6c4d
LAPTOP-3H9KC9VB\86155 <2404132126@qq.com>
2021-06-15 18:36:51 +0800

    提交

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,11 +8,27 @@ function initChart(canvas, width, height, dpr) {
     devicePixelRatio: dpr // 像素
   });
   canvas.setChart(chart);
-
-  var option = {
-  
-  };
-  chart.setOption(option);
+  var option1={
+    series : [
+      {
+        name: '访问来源',
+        type: 'pie',    // 设置图表类型为饼图
+        radius: '55%',  // 饼图的半径，外半径为可视区尺寸（容器高宽中较小一项）的 55% 长度。
+        data: [          // 数据数组，name 为数据项名称，value 为数据项值
+          {value: 240, name: '产品B'},
+          {value: 230, name: '产品C'},
+          {value: 250, name: '产品D'},
+          {value: 180, name: '产品E'},
+          {value: 100, name: '产品A'}
+        ],
+      }
+    ],
+    label:{
+      show: true,
+      formatter: '{b}:{c}({d}%)'
+}
+  }
+  chart.setOption(option1);
   return chart;
 }
 
@@ -20,7 +36,7 @@ Page({
   data: {
     ec: {
       onInit: initChart
-    }
+    },
   }
 });
 
